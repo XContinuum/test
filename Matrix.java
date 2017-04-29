8caf1bbcce5ba4dccbc41f0354884afeccd0351d
Michel Balamou <michelbalamou@michelbalamou.local>
2017-04-29 12:16:13 -0400

    Added a new class.

---
+++ Matrix.java
@@ -0,0 +1,12 @@
+public class Matrix
+{
+   private int[][] data;
+   private int rank;
+   private int rows;
+   private int columns;
+
+   public Matrix(int[][] data)
+   {
+      this.data=data;
+   }
+}
